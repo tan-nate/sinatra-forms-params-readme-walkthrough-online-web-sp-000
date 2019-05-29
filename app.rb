f774afbb8ea7671c89b75f03125c385e5c12b6ec
Nate Tan <nktan93@gmail.com>
2019-05-29 19:02:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,5 +7,9 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-
+  <form>
+    <p>Your Name: <input type="text"></p>
+    <p>Your Favorite Food: <input type="text"></p>
+    <input type="submit">
+  </form>
 end
